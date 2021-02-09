8ed00568ab0db5a1058dd517151e780664342681
Alfonso <alfonsodelag1@gmail.com>
2021-02-10 00:12:53 +0100

    adding products at commerce.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,19 +9,17 @@ const Product = ({ product }) => {
 
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5" gutterBottom>
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div>
-                <Typography variant="h2" color="textSecondary">
-                    {product.description}
-                </Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="h2" color="textSecondary" />
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
                 <IconButton aria-label="Add to Cart">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5', image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/i1-8a39d2ed-c6f8-4183-93a9-69cf4dbd0cb8/react-miler-running-shoe-Rl20rZ.jpg' },
-    { id: 2, name: 'MacBook', description: 'Apple MacBook', price: '$10', image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000' },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
